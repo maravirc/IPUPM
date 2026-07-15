0eaa24ee9ceb4f0812415954865eb06ec86ef606
arengifo <maravirc@gmail.com>
2026-07-15 16:37:56 -0500

    aleco

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "v51";
+const VERSION = "v52";
 const CACHE_NAME = "himnario-ipu-" + VERSION; // Cambia la versión cuando actualices
 
 // 📁 CORREGIDO: La ruta de los JSON debe ser data/
